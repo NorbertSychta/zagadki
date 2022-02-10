30681f4b3a3240db2e79875e9f53ecbd2e93c577
NorbertSychta <66336980+NorbertSychta@users.noreply.github.com>
2022-02-10 22:46:10 +0100

    Add files via upload

---
+++ funkcje.js
@@ -0,0 +1,40 @@
+function f1()
+{
+    document.getElementById("d1").innerHTML="Odpowiedź do zagadki pierwszej: Piąta córka miała na imię Alicja.";
+}
+function f2()
+{
+    document.getElementById("d1").innerHTML="Odpowiedź do zagadki pierwszej: Ukryto.";
+} 
+function f3()
+{
+     document.getElementById("d2").innerHTML="Odpowiedź do zagadki drugiej: Jedna bogini dostała jabłko z koszykiem.";
+}
+function f4()
+{
+    document.getElementById("d2").innerHTML="Odpowiedź do zagadki drugiej: Ukryto.";
+}   
+function f5()
+{
+    document.getElementById("d3").innerHTML="Odpowiedź do zagadki trzeciej: Jutro.";
+}  
+function f6()
+{
+    document.getElementById("d3").innerHTML="Odpowiedź do zagadki trzeciej: Ukryto.";
+}
+function f7()
+{
+    document.getElementById("d4").innerHTML="Odpowiedź do zagadki czwartej: W lutym.";
+}
+function f8()
+{
+    document.getElementById("d4").innerHTML="Odpowiedź do zagadki czwartej: Ukryto.";
+}
+function f9()
+{
+    document.getElementById("d5").innerHTML="Odpowiedź do zagadki piątej: Jedno. Potem żołądek już nie będzie pusty.";
+}
+function f10()
+{
+    document.getElementById("d5").innerHTML="Odpowiedź do zagadki piątej: Ukryto.";
+}
